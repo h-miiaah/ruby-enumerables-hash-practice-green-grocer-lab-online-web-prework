2e0c4d54430d07a98bad3e6281deaadfdc72014e
Haim D <haim11293@aol.com>
2019-07-23 01:20:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,6 +12,22 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    coupon.each do |attribute, value|
+      name = coupon[:item]
+
+      if cart[name] && cart[name][:coupon] >= coupon[:num]
+        if cart["#{name} W/COUPON"]
+          cart["#{name} W/COUPON"][:count] += 1
+        else
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearence => cart[name][:clearence], :count => 1}
+        end
+
+        cart[name][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
